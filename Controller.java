9ae582d6837a06238ea0638a7dd29d3f85352895
ZachDes07 <56686700+ZachDes07@users.noreply.github.com>
2020-02-13 15:14:44 -0500

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,4 +1,66 @@
 package sample;
 
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+
+
 public class Controller {
+    public TextField txfSource;
+    public ImageView poubTarget;
+
+
+    /**
+     * Démarre le drag
+     * @param mouseEvent
+     */
+    public void txfSourceDragDetected(MouseEvent mouseEvent) {
+        Dragboard db = txfSource.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent content = new ClipboardContent();
+        if(txfSource.getText().isEmpty()) {
+            content.putString(txfSource.getText());
+            db.setContent(content);
+        }
+    }
+
+    /**
+     * accepte le drag
+     * @param dragEvent
+     */
+    public void poubTargetDragDropped(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    /**
+     * Drop le drag
+     * @param dragEvent
+     */
+    public void poubTargetDragOver(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        txfSource.setText(db.getString());
+        txfSource.clear();
+    }
+
+    /**
+     * Démarre le drag
+     * @param mouseEvent
+     */
+    public void txfComboDragDetected(MouseEvent mouseEvent) {
+        Dragboard db = txfSource.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent content = new ClipboardContent();
+        if (txfSource.getText().isEmpty()) {
+            content.putString(txfSource.getText());
+            db.setContent(content);
+        }
+    }
+
+    public void comboDragDropped(DragEvent dragEvent) {
+        dragEvent.acceptTransferModes(TransferMode.ANY);
+    }
+
+    public void comboDragOver(DragEvent dragEvent) {
+        Dragboard db = dragEvent.getDragboard();
+        txfSource.setText(db.getString());
+        txfSource.clear();
+    }
 }
